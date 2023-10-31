62e5f82fb7d99f85adac6883ecc44d865ce70fd4
Tw1ZZLER <ilekbrownies@gmail.com>
2023-10-31 02:30:21 -0400

    CSS styling, bootstrap, and some database stuff

--- webserver.py
+++ webserver.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-import SerialMonitor
+import static.py.SerialMonitor as SerialMonitor
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def index():
